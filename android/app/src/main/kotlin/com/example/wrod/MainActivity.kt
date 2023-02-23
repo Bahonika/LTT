4310872410208b11f8c2f2de605b4334749c346e
Bahonika <bahonika1@mail.ru>
2023-02-23 21:28:05 +0300

    :tada: Init commit

---
+++ android/app/src/main/kotlin/com/example/wrod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wrod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: TextField(
+          controller: controller,
+          textInputAction: TextInputAction.none,
+          maxLines: null,
+          minLines: 1,
+          decoration: const InputDecoration.collapsed(
+            hintText: '',
+          ),
+          // scrollPadding: EdgeInsets.all(20.0),
+          autofocus: true,
+        ),
+      ),
+    );
+  }
+}
